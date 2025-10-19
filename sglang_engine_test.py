15b1f0970935e39afa194ec8b90fff181f65d86d
Thom Lake <tlake@indeed.com>
2025-10-19 12:29:05 -0500

    add test script

---
+++ sglang_engine_test.py
@@ -0,0 +1,27 @@
+print('[DO] import')
+
+import sglang
+from transformers import AutoConfig
+
+print('[DONE]')
+print('[DO] load config')
+
+model = 'McGill-NLP/delethink-24k-1.5b'
+config = AutoConfig.from_pretrained(model)
+
+print('[DONE]')
+print('[DO] load engine')
+
+llm = sglang.Engine(
+    model_path=model,
+    dtype=config.dtype,
+    tp_size=1,
+    dp_size=1,
+    trust_remote_code=True,
+    context_length=2048 + 8192,
+    attention_backend='flex_attention',
+    mem_fraction_static=0.8,
+    log_level='INFO',
+)
+
+print('[DONE]')
